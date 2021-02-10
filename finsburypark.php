fdce2c321a10af039e36c035416d87fcd13115ae
Coleman Watts <coleman@civicrm.org>
2021-02-10 11:43:23 -0500

    Add noConflict file to resolve clash between BS3 and jQuery UI
    Based on https://github.com/civicrm/civicrm-core/commit/c99c68b10f945abaf0c662dcc5a318aca1abc507

--- finsburypark.php
+++ finsburypark.php
@@ -6,7 +6,7 @@ use CRM_Finsburypark_ExtensionUtil as E;
 /**
  * Implements hook_civicrm_config().
  *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/ 
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
 function finsburypark_civicrm_config(&$config) {
   _finsburypark_civix_civicrm_config($config);
@@ -23,6 +23,9 @@ function finsburypark_civicrm_alterBundle(CRM_Core_Resources_Bundle $bundle) {
       $bundle->clear();
       $bundle->addStyleFile('finsburypark', 'css/bootstrap3.css');
       $bundle->addScriptFile('finsburypark', 'js/bootstrap.min.js', [
+        'translate' => FALSE,
+      ]);
+      $bundle->addScriptFile('finsburypark', 'js/noConflict.js', [
         'translate' => FALSE,
       ]);
       break;
